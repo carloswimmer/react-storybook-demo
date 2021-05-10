312ec83019c04a0eb386051e4dc8561a0d6790c2
Carlos Wimmer <coiote777@gmail.com>
2021-05-10 11:49:05 -0300

    Implement args mechanism

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,6 +4,9 @@ import Button from './Button'
 export default {
   title: 'Form/Button',
   component: Button,
+  args: {
+    children: 'Button',
+  },
 }
 
 export const Primary = () => <Button variant="primary">Primary</Button>
@@ -12,3 +15,23 @@ export const Success = () => <Button variant="success">Success</Button>
 export const Danger = () => <Button variant="danger">Danger</Button>
 
 Primary.storyName = 'Primary (default)'
+
+const Template = (args) => <Button {...args} />
+
+export const PrimaryArgs = Template.bind({})
+PrimaryArgs.args = {
+  variant: 'primary',
+  // children: 'Primary Args',
+}
+
+export const LongPrimaryArgs = Template.bind({})
+LongPrimaryArgs.args = {
+  ...PrimaryArgs.args,
+  children: 'Long Primary Args',
+}
+
+export const SecondaryArgs = Template.bind({})
+SecondaryArgs.args = {
+  variant: 'secondary',
+  // children: 'Secondary Args',
+}
